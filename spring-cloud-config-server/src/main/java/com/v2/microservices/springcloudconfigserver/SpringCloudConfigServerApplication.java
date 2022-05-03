ff880bc621866dd56bb66302fc612f3c2b885eaa
JuanEstebanKatz <jestebankatz@gmail.com>
2022-05-03 00:27:20 -0500

    adding logic
    logic to get properties from external git properties file

--- spring-cloud-config-server/src/main/java/com/v2/microservices/springcloudconfigserver/SpringCloudConfigServerApplication.java
+++ spring-cloud-config-server/src/main/java/com/v2/microservices/springcloudconfigserver/SpringCloudConfigServerApplication.java
@@ -2,7 +2,15 @@ package com.v2.microservices.springcloudconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+/***
+ * Para que tome la config de un server tipo Spring Cloud server es necesario 
+ * EnableConfigServer
+ * @author esteb
+ *
+ */
+@EnableConfigServer
 @SpringBootApplication
 public class SpringCloudConfigServerApplication {
 
